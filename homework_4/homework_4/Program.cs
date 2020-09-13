f770f4e9f9f0f6355f80a99bca7bc0b16198bf29
Sylvia <Sylvia@DESKTOP-2VU9OGN>
2020-09-14 01:09:13 +0800

    托普利茨矩阵

---
+++ homework_4/homework_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_4
+{
+    class Program
+    {
+        public static bool IsToplitzMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i + 1, j + 1] != matrix[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int[,] matrix = { { 1, 2, 3, 4 }, { 5, 1, 2, 3 }, { 9, 5, 1, 2 } };
+            if (IsToplitzMatrix(matrix))
+                Console.WriteLine("True.");
+            else
+                Console.WriteLine("False.");
+            Console.ReadLine();
+        }
+    }
+}
