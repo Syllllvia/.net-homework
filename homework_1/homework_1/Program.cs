ea0e3326fd66eb9bdeaea358f8b4034c0fa41367
Sylvia <Sylvia@DESKTOP-2VU9OGN>
2020-09-14 00:51:48 +0800

    min&max&sum&avg

---
+++ homework_1/homework_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("The prime factor is: ");
+            for (int i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                    n /= i;
+                    Console.WriteLine(i);
+                    
+            }
+            Console.WriteLine("Over");
+        }
+    }
+}
+
---
+++ homework_2/homework_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArr = Console.ReadLine().Split(' ');
+            int[] intArr = new int[strArr.Length];
+            intArr = Array.ConvertAll<string, int>(strArr, x => int.Parse(x));
+
+            Console.WriteLine("Min: " + intArr.Min());
+            Console.WriteLine("Max: " + intArr.Max());
+            Console.WriteLine("Sum: " + intArr.Sum());
+            Console.WriteLine("Average: " + intArr.Average());
+            Console.ReadLine();
+        }
+    }
+}
